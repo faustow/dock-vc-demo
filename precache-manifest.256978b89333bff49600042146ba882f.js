be6fb7f8de97be295acf032254c859d797fc47ae
Fausto Woelflin <faustow@gmail.com>
2020-04-22 19:38:33 -0300

    Updates

---
+++ precache-manifest.256978b89333bff49600042146ba882f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5620ff8ae0b5de477bc348425a2a6568",
+    "url": "/dock-vc-demo/index.html"
+  },
+  {
+    "revision": "c914833b1f86340da7b6",
+    "url": "/dock-vc-demo/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "26b2d46d2dece6e1b11b",
+    "url": "/dock-vc-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c914833b1f86340da7b6",
+    "url": "/dock-vc-demo/static/js/2.6281988e.chunk.js"
+  },
+  {
+    "revision": "eb069b9e34d06d1fbc22834fa9d9a818",
+    "url": "/dock-vc-demo/static/js/2.6281988e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b2d46d2dece6e1b11b",
+    "url": "/dock-vc-demo/static/js/main.8f06c24d.chunk.js"
+  },
+  {
+    "revision": "a30095fc22cf7b51245b",
+    "url": "/dock-vc-demo/static/js/runtime-main.648c3f46.js"
+  }
+]);
